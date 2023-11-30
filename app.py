2f7fd9dece3b3ab1c559837c471c03211100ccff
JUAN JOSE MELO <88802921+JuanmeloDev@users.noreply.github.com>
2023-11-30 01:11:52 +0000

    my game with python

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+import random
+wins = 0
+#pc choice
+while True :
+    List = ["rock", "paper", "scissors"]
 
-from flask import Flask
-app = Flask(__name__)
+    pc_choice = random.choice(List)
+    #print(pc_choice)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+    #user choice
+    my_choice = input("rock, paper, scissors? ").lower()
+
+
+    #game logic
+
+    def game():
+
+        global wins 
+        if pc_choice == my_choice:
+            print("It's a tie!")
+        elif pc_choice == "rock" and my_choice == "scissors":
+            print("You lose!")
+        elif pc_choice == "paper" and my_choice == "rock":
+            print("You lose!")
+        elif pc_choice == "scissors" and my_choice == "paper":
+            print("You lose!")
+        elif pc_choice == "rock" and my_choice == "paper":
+            print("You win!")
+            wins += 1
+        elif pc_choice == "paper" and my_choice == "scissors":
+            print("You win!")
+            wins += 1
+        elif pc_choice == "scissors" and my_choice == "rock":
+            print("You win!")
+            wins += 1
+
+
+    if my_choice != "rock" and my_choice != "paper" and my_choice != "scissors":
+        print("Please choose a valid option")
+    else:
+        game()
+
+    if input("Do you want to play again? (y/n) ").lower() == "y":
+        my_choice = ""
+        game()
+    else:
+        print("You won " + str(wins) + " times.")
+        print("Thanks for playing!")
+        break
